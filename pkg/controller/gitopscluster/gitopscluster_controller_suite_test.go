268ecf34997abc370057fa7df8de93ae82d99886
Roke Jung <roke@redhat.com>
2022-01-12 13:40:20 -0500

    remove deprecated migration (#20)
    * remove deprecated migration
    
    Signed-off-by: Roke Jung <roke@redhat.com>
    
    * remove unused module
    
    Signed-off-by: Roke Jung <roke@redhat.com>

--- pkg/controller/gitopscluster/gitopscluster_controller_suite_test.go
+++ pkg/controller/gitopscluster/gitopscluster_controller_suite_test.go
@@ -23,7 +23,6 @@ import (
 	"testing"
 
 	"github.com/onsi/gomega"
-	endpointapis "github.com/open-cluster-management/klusterlet-addon-controller/pkg/apis"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	spokeClusterV1 "open-cluster-management.io/api/cluster/v1"
@@ -44,7 +43,6 @@ func TestMain(m *testing.M) {
 		},
 	}
 
-	endpointapis.AddToScheme(scheme.Scheme)
 	spokeClusterV1.AddToScheme(scheme.Scheme)
 	clusterv1alpha1.AddToScheme(scheme.Scheme)
 
