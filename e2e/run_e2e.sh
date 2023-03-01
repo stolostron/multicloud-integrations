da9b44ed9a0c3e855619d3df6397a23aa172a2e0
Jonathan Marcantonio <jmarcant@redhat.com>
2023-03-01 15:25:37 -0500

    Update propagation code and enhance e2e tests (#82)
    * update propagation code with status sync (#34)
    
    Signed-off-by: Mike Ng <ming@redhat.com>
    (cherry picked from commit d7a8d52dc08c68ca185faf94b9f82aeef6e1f830)
    
    * enhance e2e with more validations (#35)
    
    Signed-off-by: Mike Ng <ming@redhat.com>
    (cherry picked from commit 3257cfbdc29e863dc483f6b3a09bd332e2c2ea6f)
    
    ---------
    
    Co-authored-by: Mike Ng <ming@redhat.com>

---
+++ e2e/run_e2e.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+###############################################################################
+# Copyright Contributors to the Open Cluster Management project
+###############################################################################
+
+set -o nounset
+set -o pipefail
+
+### Setup
+echo "SETUP install Argo CD to Managed cluster"
+kubectl config use-context kind-cluster1
+kubectl create namespace argocd
+kubectl apply -n argocd --force -f hack/test/e2e/argo-cd-install.yaml 
+
+echo "SETUP install Argo CD to Hub cluster"
+kubectl config use-context kind-hub
+kubectl create namespace argocd
+kubectl apply -n argocd --force -f hack/test/e2e/argo-cd-install.yaml 
+kubectl -n argocd scale deployment/argocd-dex-server --replicas 0
+kubectl -n argocd scale deployment/argocd-repo-server --replicas 0
+kubectl -n argocd scale deployment/argocd-server --replicas 0
+kubectl -n argocd scale deployment/argocd-redis --replicas 0
+kubectl -n argocd scale deployment/argocd-notifications-controller --replicas 0
+kubectl -n argocd scale statefulset/argocd-application-controller --replicas 0
+
+echo "SETUP install multicloud-integrations"
+kubectl config use-context kind-hub
+kubectl apply -f deploy/crds/
+kubectl apply -f deploy/controller/
+
+sleep 60s
+
+echo "SETUP print managed cluster setup"
+kubectl config use-context kind-cluster1
+kubectl -n argocd get deploy
+kubectl -n argocd get statefulset
+
+echo "SETUP print hub setup"
+kubectl config use-context kind-hub
+kubectl -n argocd get deploy
+kubectl -n argocd get statefulset
+kubectl -n open-cluster-management get deploy
+
+### GitOpsCluster
+echo "TEST GitOpsCluster"
+kubectl config use-context kind-hub
+kubectl apply -f examples/argocd/
+sleep 10s
+if kubectl -n argocd get gitopsclusters argo-ocm-importer -o yaml | grep successful; then
+    echo "GitOpsCluster: status successful"
+else
+    echo "GitOpsCluster FAILED: status not successful"
+    exit 1
+fi
+if kubectl -n argocd get secret cluster1-cluster-secret; then
+    echo "GitOpsCluster: cluster1-cluster-secret created"
+else
+    echo "GitOpsCluster FAILED: cluster1-cluster-secret not created"
+    exit 1
+fi
+
+### Propagation
+echo "TEST Propagation"
+kubectl config use-context kind-cluster1
+kubectl apply -f e2e/managed/
+kubectl config use-context kind-hub
+kubectl apply -f e2e/hub/
+sleep 30s
+if kubectl -n argocd get application cluster1-guestbook-app; then
+    echo "Propagation: hub application cluster1-guestbook-app created"
+else
+    echo "Propagation FAILED: hub application cluster1-guestbook-app not created"
+    exit 1
+fi
+if kubectl -n cluster1 get manifestwork | grep cluster1-guestbook-app; then
+    echo "Propagation: manifestwork created"
+else
+    echo "Propagation FAILED: manifestwork not created"
+    exit 1
+fi
+if kubectl -n argocd get app cluster1-guestbook-app | grep Synced | grep Healthy; then
+    echo "Propagation: hub cluster application cluster1-guestbook-app status sync"
+else
+    echo "Propagation FAILED: hub application cluster1-guestbook-app not status sync"
+    exit 1
+fi
+if kubectl -n argocd get application cluster1-guestbook-app -o yaml | grep AdditionalStatusReport; then
+    echo "Propagation: hub application cluster1-guestbook-app contains AdditionalStatusReport"
+else
+    echo "Propagation FAILED: hub application cluster1-guestbook-app not contains AdditionalStatusReport"
+    exit 1
+fi
+kubectl config use-context kind-cluster1
+if kubectl -n argocd get app cluster1-guestbook-app | grep Synced | grep Healthy; then
+    echo "Propagation: managed cluster application cluster1-guestbook-app created, synced and healthy"
+else
+    echo "Propagation FAILED: managed application cluster1-guestbook-app not created, synced and healthy"
+    exit 1
+fi
+if kubectl get namespace guestbook; then
+    echo "Propagation: guestbook namespace created"
+else
+    echo "Propagation FAILED: guestbook namespace not created"
+    exit 1
+fi
+if kubectl -n guestbook get deploy guestbook-ui; then
+    echo "Propagation: guestbook-ui deploy created"
+else
+    echo "Propagation FAILED: guestbook-ui deploy not created"
+    exit 1
+fi
